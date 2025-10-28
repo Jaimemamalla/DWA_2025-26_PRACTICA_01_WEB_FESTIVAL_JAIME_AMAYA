14f35620e8faf6d95b4c3a95dbf110f4fd6ae9e6
Jaime <jaimeamayasanchez02@gmail.com>
2025-10-28 23:53:02 +0100

    navbar&menuham done

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,1 +1,10 @@
-console.log("Hola esto es una prueba");
+const burger = document.querySelector('.burger i');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('fa-bars');
+    burger.classList.toggle('fa-times');
+});
+
+
