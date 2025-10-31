f631f7e783b5605e424177b951437e719546368f
Jaime <jaimeamayasanchez02@gmail.com>
2025-10-31 01:28:22 +0100

    resto de secciones&artistas wip

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//MENU HAM
+
 const burger = document.querySelector('.burger i');
 const navLinks = document.querySelector('.nav-links');
 
@@ -7,4 +9,37 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('fa-times');
 });
 
+//MODAL 
 
+// MODAL PAGO
+const modal = document.getElementById('modal-pago');
+const closeModal = document.querySelector('.close');
+const ticketTexto = document.getElementById('ticket-seleccionado');
+const formPago = document.getElementById('form-pago');
+
+// ABRIR MODAL
+document.querySelectorAll('.btn-comprar').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const tipo = btn.dataset.ticket;
+    ticketTexto.textContent = `Has seleccionado: ${tipo}`;
+    modal.style.display = 'flex';
+  });
+});
+
+// CERRAR MODAL
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// CERRAR AL HACER CLICK FUERA DEL MODAL
+window.addEventListener('click', e => {
+  if (e.target === modal) modal.style.display = 'none';
+});
+
+// OKEY DE COMPRA
+formPago.addEventListener('submit', e => {
+  e.preventDefault();
+  alert('¡Compra realizada con éxito! 🎟️');
+  modal.style.display = 'none';
+  formPago.reset();
+});
